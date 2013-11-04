8d3dfe85e8bc44d138eedbaa4632ec0743a78a9c
windvix <windvix@foxmail.com>
2013-11-04 16:33:29 +0800

    2013/11/4 16:33

--- src/com/athudong/video/MainActivityCircle.java
+++ src/com/athudong/video/MainActivityCircle.java
@@ -1,11 +1,13 @@
 package com.athudong.video;
 
+import android.content.Intent;
+import android.sax.StartElementListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AbsoluteLayout.LayoutParams;
 import android.widget.LinearLayout;
 
-public class MainActivityCircle implements OnClickListener{
+public class MainActivityCircle implements OnClickListener {
 
 	private MainActivity act;
 	private View root;
@@ -19,36 +21,21 @@ public class MainActivityCircle implements OnClickListener{
 		root.findViewById(R.id.circle_layout_04).setOnClickListener(this);
 	}
 
-	
-
 	@Override
 	public void onClick(View v) {
-		
+
 		int id = v.getId();
-		act.toast(id+"");
-		
+
+		if (id == R.id.circle_layout_01) {
+			Intent intent = new Intent(act, ZoneActivity.class);
+			act.startActivity(intent);
+		} else {
+
+			act.toast(id + "内测中");
+		}
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	private int top;
 
 	private float r;
@@ -58,38 +45,35 @@ public class MainActivityCircle implements OnClickListener{
 		View bigCircleLayout = root.findViewById(R.id.bigCircleLayout);
 		top = bigCircleLayout.getTop();
 		r = bigCircleLayout.getHeight() / 2.0f;
-		smallCircleR = root.findViewById(R.id.circle_layout_01).getHeight()/2.0f;
-		mainWidth1 = sin36*r;
-		mainHeight1 = cos36*r;
-		mainWidth2 = cos18*r;
-		mainHeight2 = sin18*r;
+		smallCircleR = root.findViewById(R.id.circle_layout_01).getHeight() / 2.0f;
+		mainWidth1 = sin36 * r;
+		mainHeight1 = cos36 * r;
+		mainWidth2 = cos18 * r;
+		mainHeight2 = sin18 * r;
 		setCircle1();
 		setCircle2();
 		setCircle3();
 		setCircle4();
-		//设置为不可见
+		// 设置为不可见
 		root.findViewById(R.id.circle_layout_01).setVisibility(View.INVISIBLE);
 		root.findViewById(R.id.circle_layout_02).setVisibility(View.INVISIBLE);
 		root.findViewById(R.id.circle_layout_03).setVisibility(View.INVISIBLE);
 		root.findViewById(R.id.circle_layout_04).setVisibility(View.INVISIBLE);
-	
 
 	}
-	
-	
-	
+
 	private float smallCircleR = 0;
 	private float mainWidth1 = 0;
 	private float mainHeight1 = 0;
-	
+
 	private float mainWidth2 = 0;
 	private float mainHeight2 = 0;
-	
+
 	private float sin36 = 0.587785f;
 	private float sin18 = 0.309017f;
 	private float cos36 = 0.80901699f;
 	private float cos18 = 0.9510565f;
-	
+
 	private void setCircle1() {
 		final View view = root.findViewById(R.id.circle_layout_01);
 		view.post(new Runnable() {
@@ -97,14 +81,13 @@ public class MainActivityCircle implements OnClickListener{
 			public void run() {
 				@SuppressWarnings("deprecation")
 				LayoutParams lp = (LayoutParams) view.getLayoutParams();
-				lp.x = (int) (mainWidth1-smallCircleR);
-				lp.y = (int) (top+r-mainHeight1-smallCircleR);
+				lp.x = (int) (mainWidth1 - smallCircleR);
+				lp.y = (int) (top + r - mainHeight1 - smallCircleR);
 				view.setLayoutParams(lp);
 			}
 		});
 	}
 
-	
 	private void setCircle2() {
 		final View view = root.findViewById(R.id.circle_layout_02);
 		view.post(new Runnable() {
@@ -112,8 +95,8 @@ public class MainActivityCircle implements OnClickListener{
 			public void run() {
 				@SuppressWarnings("deprecation")
 				LayoutParams lp = (LayoutParams) view.getLayoutParams();
-				lp.x = (int) (mainWidth2-smallCircleR);
-				lp.y = (int) (top+r-mainHeight2-smallCircleR);
+				lp.x = (int) (mainWidth2 - smallCircleR);
+				lp.y = (int) (top + r - mainHeight2 - smallCircleR);
 				view.setLayoutParams(lp);
 			}
 		});
@@ -126,8 +109,8 @@ public class MainActivityCircle implements OnClickListener{
 			public void run() {
 				@SuppressWarnings("deprecation")
 				LayoutParams lp = (LayoutParams) view.getLayoutParams();
-				lp.x = (int) (mainWidth2-smallCircleR);
-				lp.y = (int) (top+r+mainHeight2-smallCircleR);
+				lp.x = (int) (mainWidth2 - smallCircleR);
+				lp.y = (int) (top + r + mainHeight2 - smallCircleR);
 				;
 				view.setLayoutParams(lp);
 			}
@@ -141,12 +124,11 @@ public class MainActivityCircle implements OnClickListener{
 			public void run() {
 				@SuppressWarnings("deprecation")
 				LayoutParams lp = (LayoutParams) view.getLayoutParams();
-				lp.x = (int) (mainWidth1-smallCircleR);
-				lp.y = (int) (top+r+mainHeight1-smallCircleR);
+				lp.x = (int) (mainWidth1 - smallCircleR);
+				lp.y = (int) (top + r + mainHeight1 - smallCircleR);
 				view.setLayoutParams(lp);
 			}
 		});
 	}
-
 
 }
---
+++ src/com/athudong/video/ZoneActivity.java
@@ -0,0 +1,34 @@
+package com.athudong.video;
+
+import android.os.Bundle;
+
+import com.athudong.video.task.BaseTask;
+
+public class ZoneActivity extends BaseActivity {
+
+	@Override
+	protected void initView(Bundle savedInstanceState) {
+		
+	}
+
+	@Override
+	protected void beforeEveryVisable() {
+		
+	}
+
+	@Override
+	protected void afterEveryInVisable() {
+		
+	}
+
+	@Override
+	protected void beforeDestory() {
+	
+	}
+
+	@Override
+	public void executeTaskResult(BaseTask task, Object data) {
+		
+	}
+
+}
